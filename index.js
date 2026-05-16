846960f263f2e97ca76804e79de66f37217c7037
jahid3741 <mdj868445@gmail.com>
2026-05-16 06:38:14 +0600

    logic of code

--- index.js
+++ index.js
@@ -19,3 +19,28 @@ app.use(express.json());
 app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+// Verify JWT token middleware
+function verifyToken(req, res, next) {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+}
